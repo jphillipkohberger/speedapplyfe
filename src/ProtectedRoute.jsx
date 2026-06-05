4b1aefb52419c8109d057be475a1fc5fe3becbd2
Phillip Kohberger <jphillipkohberger@yahoo.com>
2026-06-05 15:21:33 -0400

    Force redirect to Profile if address, minsal or files missing

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,13 +1,22 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthProvider.jsx";
+import { useLocation } from 'react-router-dom';
 
 // Wrap protected pages with this to block access if not logged in
 export default function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { user, isAuthenticated } = useAuth();
+  const location = useLocation();
 
   if (!isAuthenticated) {
     return <Navigate to="/Login" replace />;
   }
+
+  if(!location.pathname.includes("/Profile") && (!user.address || !user.minSal || user.files.length == 0)) {
+    console.log("no user");
+    return <Navigate to="/Profile" replace />;
+  }
+  console.log("there is a user")
+  console.log(user);
 
   return children;
 }
